d5c9ab403b1a5e5cd796ed7dc6198faac343d80b
Daniel Sánchez <dani9oo.dev@gmail.com>
2022-06-28 20:40:37 -0500

    Added dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'landing', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) }];
+const routes: Routes = [{ path: 'landing', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) }, { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
