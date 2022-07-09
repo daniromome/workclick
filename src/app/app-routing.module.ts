03cc85a29e315969bc24a4a05fb4025fd83afd5b
Daniel Sánchez <dani9oo.dev@gmail.com>
2022-07-08 19:32:07 -0500

    Progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './shared/components/auth/auth.component';
 
-const routes: Routes = [{ path: 'landing', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) }, { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }];
+const routes: Routes = [
+  { path: 'landing', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) },
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'auth', component: AuthComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
