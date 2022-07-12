4ec95545945b38664ca2e0aabf14e8d37aec3f84
taniajimarq <taniajimarq.utj@gmail.com>
2022-07-12 17:22:03 -0500

    info pages

--- src/app/modules/info-free/info-free.module.ts
+++ src/app/modules/info-free/info-free.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { InfoFreeRoutingModule } from './info-free-routing.module';
 import { InfoFreeComponent } from './info-free.component';
-
+import { HeaderModule } from '../header/header.module';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,11 @@ import { InfoFreeComponent } from './info-free.component';
   ],
   imports: [
     CommonModule,
-    InfoFreeRoutingModule
+    InfoFreeRoutingModule,
+    HeaderModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class InfoFreeModule { }
